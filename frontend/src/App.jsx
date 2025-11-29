217d10d1c28112f892beb5e7ab6fc90576f33f16
Prince Bhattrai <bhattraiprince39@gmail.com>
2025-11-29 22:00:35 +0545

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { io } from 'socket.io-client'
 
-const socket = io("http://localhost:4000");
+const socket = io("https://easy-chat-9x5m.onrender.com");
 
 
 
